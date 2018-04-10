95c3c20b9d0a7ca99b1b6f1cd0ae3f24bfc25a06
bclin <bclin@coremail.cn>
2018-04-10 17:47:11 +0800

    Initial commit

---
+++ src/main/java/top/bingchenglin/wxgzh/WxgzhApplication.java
@@ -0,0 +1,12 @@
+package top.bingchenglin.wxgzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxgzhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxgzhApplication.class, args);
+	}
+}
