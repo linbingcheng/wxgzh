9c6ec110744b70b823abd98380aadd3cba2a30fc
bclin <bclin@coremail.cn>
2018-04-10 18:25:15 +0800

    基础框架搭建

--- src/main/java/top/bingchenglin/wxgzh/WxgzhApplication.java
+++ src/main/java/top/bingchenglin/wxgzh/WxgzhApplication.java
@@ -1,9 +1,17 @@
 package top.bingchenglin.wxgzh;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableAsync
+@EnableTransactionManagement
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "top.bingchenglin.wxgzh")
 public class WxgzhApplication {
 
 	public static void main(String[] args) {
